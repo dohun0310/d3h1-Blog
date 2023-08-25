0ef7b0a1c771ae8636b0eb5c879c31ee92453ed7
dohun0310 <dohun0310@naver.com>
2023-08-25 19:17:12 +0900

    edit next-sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: "https://blog.d3h1.com",
-    generateRobotsTxt: true,
-  }
+  siteUrl: process.env.SITE_URL || "https://blog.d3h1.com",
+  generateRobotsTxt: true,
+  priority: 1.0,
+  generateIndexSitemap: false,
+}
