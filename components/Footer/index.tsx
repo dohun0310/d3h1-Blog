90cf8b3bbf3a6ce2831a6646d0bc252d4ec82ee2
김도훈 <dohun0310@naver.com>
2025-10-29 15:36:37 +0900

    [Refactor/#4] Refactor: `Button` 컴포넌트 추가 및 `Footer`와 `PostList`에서 사용하도록 수정

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import Icon from "../Icon";
+import Button from "../Button";
 import styles from "./footer.module.css";
 
 export default function Footer() {
@@ -7,28 +7,40 @@ export default function Footer() {
     <footer className={styles.footer}>
       <div className={styles.menu}>
         <Link href="https://www.instagram.com/dohun0310/">
-          <div className={styles.item}>
-            <Icon name="instagram"/>
-          </div>
+          <Button
+            size="tiny"
+            variant="linear"
+            iconName="instagram"
+            iconOnly
+          />
         </Link>
         <Link href="https://www.facebook.com/dohun0310/">
-          <div className={styles.item}>
-            <Icon name="facebook"/>
-          </div>
+          <Button
+            size="tiny"
+            variant="linear"
+            iconName="facebook"
+            iconOnly
+          />
         </Link>
         <Link href="https://x.com/dohun0310/">
-          <div className={styles.item}>
-            <Icon name="x"/>
-          </div>
+          <Button
+            size="tiny"
+            variant="linear"
+            iconName="x"
+            iconOnly
+          />
         </Link>
         <Link href="https://github.com/dohun0310/">
-          <div className={styles.item}>
-            <Icon name="github"/>
-          </div>
+          <Button
+            size="tiny"
+            variant="linear"
+            iconName="github"
+            iconOnly
+          />
         </Link>
       </div>
       <div className={styles.ad} />
-      <p>© 2023-2024 d3h1. 모든 권리 보유.</p>
+      <p>© 2023-2026 d3h1. 모든 권리 보유.</p>
     </footer>
   )
 }
