f5855b5228228eddbec4c185f924234e872b4f79
김도훈 <dohun0310@naver.com>
2025-10-29 21:16:08 +0900

    [Refactor/#7] Chore: `MDX` 구성 요소 추가 및 `Next.js` 설정 업데이트

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx"
 
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: ["ts", "tsx", "md"]
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  extension: /posts\/.*\.md$/,
+  options: {
+    remarkPlugins: [
+      "remark-frontmatter",
+      ["remark-mdx-frontmatter", { name: "meta" }],
+    ],
+    rehypePlugins: []
+  }
+});
+
+export default withMDX(nextConfig);
