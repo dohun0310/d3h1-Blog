3a4d6e77127888b517711e1c907579737fca71dc
김도훈 <dohun0310@naver.com>
2025-12-30 01:39:49 +0900

    [Design/#23] Refactor: `Header` 컴포넌트에서 CSS 모듈 제거 및 Tailwind CSS 클래스 적용

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -4,14 +4,19 @@ import Link from "next/link";
 import Logo from "../Logo";
 import Icon from "../Icon";
 import { useSearch } from "@/contexts/SearchContext";
-import styles from "./header.module.css";
 
 export default function Header() {
   const { openSearch } = useSearch();
 
   return (
-    <header className={styles.header}>
-      <div className={styles.container}>
+    <header className="w-full h-15 lg:h-16 top-0 left-0
+      fixed z-20 bg-background
+      border-b border-gray-100 dark:border-gray-800"
+    >
+      <div className="w-full max-w-[1325px] h-full
+        relative mx-auto px-4
+        flex items-center justify-between"
+      >
         <Link
           href="/"
           aria-label="logo"
@@ -20,11 +25,22 @@ export default function Header() {
         </Link>
         <button
           onClick={openSearch}
-          className={styles.search}
+          className="select-none w-6 lg:w-78
+            p-0 lg:px-4 lg:py-2 gap-2.5
+            flex items-center rounded-full
+            lg:border border-gray-200 dark:border-gray-800
+            hover:bg-gray-100 dark:hover:bg-gray-900"
           aria-label="search"
         >
-          <Icon name="search" />
-          <p>검색...</p>
+          <Icon
+            name="search"
+            className="fill-foreground lg:fill-gray-500 dark:lg:fill-gray-400"
+          />
+          <p className="hidden lg:block
+            text-sm text-gray-500 dark:text-gray-400
+          ">
+            검색...
+          </p>
         </button>
       </div>
     </header>
