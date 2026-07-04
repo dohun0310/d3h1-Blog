3a905fbd4dd899671b35931f86ed510885c4687d
김도훈 <dohun0310@naver.com>
2026-07-04 14:45:44 +0900

    [Fix/#28] Refactor: `@giscus/react` 의존성 사용으로 `Giscus` 컴포넌트 간소화

--- components/Giscus/index.tsx
+++ components/Giscus/index.tsx
@@ -1,34 +1,19 @@
-"use client"
+import Giscus from '@giscus/react';
 
-import { memo, useEffect, useRef } from "react";
-
-export default memo(function Giscus() {
-  const giscusContainerRef = useRef<HTMLElement | null>(null);
-
-  useEffect(() => {
-    const container = giscusContainerRef.current;
-    if (!container) return;
-
-    const scriptElement = document.createElement("script");
-    scriptElement.src = "https://giscus.app/client.js";
-    scriptElement.async = true;
-    scriptElement.setAttribute("data-repo", "dohun0310/d3h1-Blog-comment");
-    scriptElement.setAttribute("data-repo-id", "R_kgDOJ-0RuA");
-    scriptElement.setAttribute("data-category", "General");
-    scriptElement.setAttribute("data-category-id", "DIC_kwDOJ-0RuM4CYGS5");
-    scriptElement.setAttribute("data-mapping", "pathname");
-    scriptElement.setAttribute("data-reactions-enabled", "1");
-    scriptElement.setAttribute("data-emit-metadata", "0");
-    scriptElement.setAttribute("data-input-position", "bottom");
-    scriptElement.setAttribute("data-theme", "preferred_color_scheme");
-    scriptElement.setAttribute("data-lang", "ko");
-    scriptElement.setAttribute("crossorigin", "anonymous");
-    container.appendChild(scriptElement);
-
-    return () => {
-      container.innerHTML = "";
-    };
-  }, []);
-
-  return <section ref={giscusContainerRef} />;
-});
+export default function Comment() {
+  return (
+    <Giscus
+      id="comments"
+      repo="dohun0310/d3h1-Blog-comment"
+      repoId="R_kgDOJ-0RuA"
+      category="General"
+      categoryId="DIC_kwDOJ-0RuM4CYGS5"
+      mapping="pathname"
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="bottom"
+      theme="preferred_color_scheme"
+      lang="ko"
+    />
+  )
+}
