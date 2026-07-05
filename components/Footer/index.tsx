a17c9bc7abdea8b77e2445de7534419326965adc
김도훈 <dohun0310@naver.com>
2026-07-05 22:16:30 +0900

    [Design/#76] Design: 푸터 컴포넌트의 레이아웃을 수정하여 반응형 디자인 개선

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
   return (
     <footer className="w-full max-w-60 mx-auto pt-4 lg:pt-12
       flex flex-col items-center gap-8
-      static lg:fixed lg:right-[calc((100%-1325px)/2+16px)]"
+      static md:sticky md:top-24 md:self-start"
     >
       <div className="w-full flex items-center justify-center gap-4 lg:justify-between">
         {siteConfig.socials.map((link) => (
@@ -26,12 +26,12 @@ export default function Footer() {
           </a>
         ))}
       </div>
-      <div className="w-full max-w-80 lg:max-w-40
+      <div className="w-full max-w-80 md:max-w-40
         bg-gray-400 dark:bg-gray-700 object-contain
-        aspect-32/5 lg:aspect-1/3.75
-        [@media(min-width:1325px)_and_(max-height:867px)_and_(min-height:401px)]:aspect-6/5
-        [@media(min-width:1325px)_and_(max-height:400px)_and_(min-height:318px)]:aspect-32/10
-        [@media(min-width:1325px)_and_(max-height:317px)]:aspect-32/5"
+        aspect-32/5 md:aspect-1/3.75
+        [@media(min-width:674px)_and_(max-height:867px)_and_(min-height:401px)]:aspect-6/5
+        [@media(min-width:674px)_and_(max-height:400px)_and_(min-height:318px)]:aspect-32/10
+        [@media(min-width:674px)_and_(max-height:317px)]:aspect-32/5"
       />
       <p className="text-xs lg:text-sm text-gray-500 dark:text-gray-400">
         {siteConfig.copyright}
