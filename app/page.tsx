59b20d32f8757282ce659c3a6e0ecd07f8bf680d
김도훈 <dohun0310@naver.com>
2026-07-05 19:26:22 +0900

    [Refactor/#64] Refactor: `PostCard` 컴포넌트를 사용하여 포스트 렌더링 로직 간소화

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import Link from "next/link";
-import Image from "next/image";
-
 import Button from "@/components/Button";
+import PostCard from "@/components/PostCard";
 import { allPosts } from "@/lib/utils/post";
 import { categories, ALL_CATEGORY_LABEL } from "@/lib/config/categories";
-import Post from "@/lib/types/post";
 
 export default async function Home({
   searchParams
@@ -54,43 +52,12 @@ export default async function Home({
       <div className="grid grid-cols-1 lg:grid-cols-2
         gap-x-4 gap-y-5"
       >
-        {posts.map((post: Post, index: number) => (
-          <article
+        {posts.map((post, index) => (
+          <PostCard
             key={post.slug}
-            className="flex flex-col"
-          >
-            <Link
-              href={`/${post.slug}`}
-              className="text-inherit no-underline
-              transition-colors duration-300
-              hover:p-2 hover:-m-2 hover:rounded-2xl hover:bg-foreground/5"
-            >
-              <Image
-                src={post.teaser}
-                alt={post.title}
-                width={640}
-                height={360}
-                sizes="(max-width: 684px) 100vw,
-                      (max-width: 1324px) 50vw,
-                      33vw"
-                className="w-full h-auto rounded-lg mb-2"
-                fetchPriority={index < 4 ? "high" : "low"}
-                loading={index < 4 ? "eager" : "lazy"}
-                priority={index < 4}
-              />
-              <p className="text-2xs lg:text-xs pb-1">
-                {post.category}
-              </p>
-              <h1 className="text-sm lg:text-base font-bold pb-1">
-                {post.title}
-              </h1>
-              <p className="text-xs lg:text-sm
-                line-clamp-2 text-gray-500 dark:text-gray-400"
-              >
-                {post.excerpt}
-              </p>
-            </Link>
-          </article>
+            post={post}
+            priority={index < 4}
+          />
         ))}
       </div>
     </div>
