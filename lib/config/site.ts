57b2bc73f019844149f35284299e53dc05d0b6f7
김도훈 <dohun0310@naver.com>
2026-07-05 17:26:52 +0900

    [Refactor/#60] Feat: `site.ts` 파일 추가 및 사이트 구성 인터페이스 정의

---
+++ lib/config/site.ts
@@ -0,0 +1,47 @@
+import type { IconName } from "@/lib/types/icon";
+
+export interface SocialLink {
+  name: IconName;
+  href: string;
+}
+
+export interface SiteConfig {
+  url: string;
+  name: string;
+  description: string;
+  author: {
+    firstName: string;
+    lastName: string;
+  };
+  copyright: string;
+  socials: SocialLink[];
+  giscus: {
+    repo: `${string}/${string}`;
+    repoId: string;
+    category: string;
+    categoryId: string;
+  };
+}
+
+export const siteConfig: SiteConfig = {
+  url: "https://blog.d3h1.com",
+  name: "d3h1 Blog",
+  description: "새로운 것을 즐기고, 변화를 만들고",
+  author: {
+    firstName: "도훈",
+    lastName: "김",
+  },
+  copyright: "© 2023-2026 d3h1. 모든 권리 보유.",
+  socials: [
+    { name: "instagram", href: "https://www.instagram.com/dohun0310/" },
+    { name: "facebook", href: "https://www.facebook.com/dohun0310/" },
+    { name: "x", href: "https://x.com/dohun0310/" },
+    { name: "github", href: "https://github.com/dohun0310/" },
+  ],
+  giscus: {
+    repo: "dohun0310/d3h1-Blog-comment",
+    repoId: "R_kgDOJ-0RuA",
+    category: "General",
+    categoryId: "DIC_kwDOJ-0RuM4CYGS5",
+  },
+};
