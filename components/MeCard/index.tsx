cccc23c1f69e09d6b6d74ea4b49b08fa6ebbe59a
김도훈 <dohun0310@naver.com>
2026-07-05 23:15:09 +0900

    [Refactor/#81] Feat: `MeCard` 컴포넌트 추가

---
+++ components/MeCard/index.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import type { Post } from "@/lib/utils/post";
+import { formatDate } from "@/lib/utils/date";
+import { siteConfig } from "@/lib/config/site";
+
+export interface MeCardProps {
+  post: Post;
+}
+
+export default function MeCard({
+  post
+}: MeCardProps) {
+  return (
+    <div className="flex items-center gap-3
+      lg:flex-col lg:items-start lg:gap-2"
+    >
+      <Image
+        src="/profile.png"
+        alt="d3h1 Profile Image"
+        width={128}
+        height={128}
+        className="w-12 h-12 lg:w-25 lg:h-25
+          rounded-full object-cover"
+      />
+      <p className="text-sm lg:text-base font-bold">
+        {siteConfig.author.lastName} {siteConfig.author.firstName}
+      </p>
+      <time className="text-xs lg:text-sm text-gray-500 dark:text-gray-400 break-keep" dateTime={post.date}>
+        {formatDate(post.date)}
+      </time>
+      <p className="text-xs lg:text-sm text-gray-500 dark:text-gray-400 break-keep">
+        {post.category}
+      </p>
+    </div>
+  );
+}
