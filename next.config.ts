2df8773b9e223eedbc565656d0cfb0deb35a301e
김도훈 <dohun0310@naver.com>
2026-09-12 23:54:00 +0900

    [Feat/#109] Setting: Next.js standalone 출력 설정

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import createMDX from "@next/mdx"
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
   images: {
     formats: ["image/avif", "image/webp"]
   },
